80f6416b60c656944d6f79edcb7f533d97f279b0
engineersbabuxtract-sys <engineersbabuxtract@gmail.com>
2026-08-08 03:12:25 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "21705536"))
 API_HASH = os.environ.get("API_HASH", "c5bb241f6e3ecf33fe68a444e288de2d")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-WEBHOOK = os.environ.get("WEBHOOK", False)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8845555323:AAEUvHekI8V1EXf-L6oCDxHEFD1bcInYGVw")
+AUTH_USERS = os.environ.get("AUTH_USERS", "1147534909,5957208798")
+WEBHOOK = os.environ.get("WEBHOOK", "false").lower() == "true"
 PORT = int(os.environ.get("PORT", 8080))
